a37e378b79e750ca8bb02fbed5c4d9df61ceed87
username98 <ay.amouch@gmail.com>
2023-03-01 06:12:01 +0000

    first commit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function startGame() {
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+        cardsEl.textContent += " | " + cards[i] + " |"
     }
     
     sumEl.textContent = "Sum: " + sum
